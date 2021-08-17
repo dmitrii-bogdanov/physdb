1de4527f837526f3a64ecac559f98bbdc6b37a02
dmitrii-bogdanov <d.bogdanov@spbu.ru>
2021-08-17 23:03:39 +0300

    .

--- src/main/java/bogdanov/physdb/configs/WebSecurityConfig.java
+++ src/main/java/bogdanov/physdb/configs/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package bogdanov.physdb.configs;
 import bogdanov.physdb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,7 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable();
         httpSecurity.authorizeRequests().antMatchers("/**").permitAll().and().formLogin();
+        httpSecurity.authorizeRequests().antMatchers(HttpMethod.POST, "/**").permitAll().and().formLogin();
     }
 
 
